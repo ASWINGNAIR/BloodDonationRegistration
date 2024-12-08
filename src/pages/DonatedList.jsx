10a0434033f380ea0b077747ee513a4aa75eb48a
ASWINGNAIR <aswingnair17@gmail.com>
2024-12-08 20:37:00 +0530

    footer,regList,donList added

--- src/pages/DonatedList.jsx
+++ src/pages/DonatedList.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
-function DonatedList() {
+function RegisteredList() {
   return (
-    <div>DonatedList</div>
+    <>
+      <div className='py-5 px-4'>
+        <h1 className='text-danger text-center text-4xl mb-5 px-3  py-5'>Donated List</h1>
+        <div className=''>
+          <table className=' w-100 border border-secondary'>
+            <thead>
+              <tr>
+                <th className='border border-secondary bg-danger text-white'>#</th>
+                <th className='border border-secondary bg-danger text-white'>Name</th>
+                <th className='border border-secondary bg-danger text-white'>Age</th>
+                <th className='border border-secondary bg-danger text-white'>Blood Group</th>
+                <th className='border border-secondary bg-danger text-white'>Location</th>
+                <th className='border border-secondary bg-danger text-white'>Contact</th>
+                <th className='border border-secondary bg-danger text-white'>Date of Donation</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className='border border-secondary'>1</td>
+                <td className='border border-secondary'>Aswin</td>
+                <td className='border border-secondary'>23</td>
+                <td className='border border-secondary'>B+</td>
+                <td className='border border-secondary'>Alappuzha</td>
+                <td className='border border-secondary'>8976357289</td>
+                <td className='border border-secondary'>15/11/2024</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default DonatedList
+export default RegisteredList
--- src/pages/RegisteredList.jsx
+++ src/pages/RegisteredList.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function RegisteredList() {
   return (
-    <div>RegisteredList</div>
+    <>
+      <div className='pt-5'>
+        <h1 className='text-danger text-center text-4xl mb-5  py-5'>Registered List</h1>
+        <div className=''>
+          <table className='w-100 border border-secondary mb-5'>
+            <thead>
+              <tr>
+                <th className='border border-secondary bg-danger text-white'>#</th>
+                <th className='border border-secondary bg-danger text-white'>Name</th>
+                <th className='border border-secondary bg-danger text-white'>Age</th>
+                <th className='border border-secondary bg-danger text-white'>Blood Group</th>
+                <th className='border border-secondary bg-danger text-white'>Location</th>
+                <th className='border border-secondary bg-danger text-white'>Contact</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className='border border-secondary'>1</td>
+                <td className='border border-secondary'>Aswin</td>
+                <td className='border border-secondary'>23</td>
+                <td className='border border-secondary'>B+</td>
+                <td className='border border-secondary'>Alappuzha</td>
+                <td className='border border-secondary'>8976357289</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   )
 }
 
